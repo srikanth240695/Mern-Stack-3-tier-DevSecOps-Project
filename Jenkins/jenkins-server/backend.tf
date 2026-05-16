a7a1b12bef3acef3e44a0f607df965eccd496271
srikanth240695 <77737462+srikanth240695@users.noreply.github.com>
2026-05-16 11:53:29 +0530

    Set up S3 backend for Terraform state management
    Configure S3 backend for Terraform with bucket and locking.

--- Jenkins/jenkins-server/backend.tf
+++ Jenkins/jenkins-server/backend.tf
@@ -6,5 +6,11 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "srikanth-mern-stack-backend-2026"
+    key            = "terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "mern-lock-table"
+    encrypt        = true
+  }
 }
